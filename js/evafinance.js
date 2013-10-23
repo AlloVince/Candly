93ccd298751a702bd020013785da8e8e8df2547c
AlloVince <allo.vince@gmail.com>
2013-10-23 18:41:05 +0800

    basic area

--- js/evafinance.js
+++ js/evafinance.js
@@ -1,4 +1,7 @@
 function p(a){
+    if(typeof console === 'undefined') {
+        return false;
+    }
     console.log(a);
 }
 
@@ -16,24 +19,33 @@ function p(a){
 
     var evafinance
         , VERSION = '1.0.0'
+        , namespace = null
         , options = {
             container : null,
             timezoneOffset : 0,
             width : 0,
             height : 0,
-            marginLeft : 10,
-            marginRight: 80,
-            marginTop : 10,
-            marginBottom : 20,
+            marginLeft : 6,
+            marginRight: 50,
+            marginTop : 6,
+            marginBottom : 12,
+
+            //x axis
             xAxisStroke : '#CCC',
             xAxisShapeRendering : 'crispEdges',
             xAxisFill : 'none',
             xAxisTicks : 5,
+            xAxisTickSize : 0,
             xAxisOrient : 'bottom',
+            xAxisLabelSize : 12,
+            xAxisLabelColor : '#999',
+
+            //y axis
             yAxisStroke : '#CCC',
             yAxisShapeRendering : 'crispEdges',
             yAxisFill : 'none',
             yAxisTicks : 5,
+            yAxisTickSize : 0,
             yAxisOrient : 'right',
             xGridStroke : '#EEE',
             xGridShapeRendering : 'crispEdges',
@@ -43,20 +55,33 @@ function p(a){
             yGridShapeRendering : 'crispEdges',
             yGridFill : 'none',
             yGridTicks : 5,
+            yAxisLabelSize : 12,
+            yAxisLabelColor : '#999',
+
+            //area chart
+            areaFillEnable : true,
             areaFillColor : '#FFCCB8',
             areaFillOpacity : 0.8,
+            areaLineEnable : true,
             areaLineColor : '#F9653C',
-            areaLineWeight : '2px',
-            areaPointColor : '#F9653C',
-            areaPointFillColor : '#FFF',
-            areaPointSize : '3px',
-            areaPointWeight : '2px',
+            areaLineWidth : 2,
+            areaPointEnable : true,
+            areaPointStroke : '#F9653C',
+            areaPointStrokeWidth : 2,
+            areaPointFill : '#FFF',
+            areaPointSize : 3,
+            areaPointWeight : 2,
+
+
+            //candle chart
             xColor : '#CCC',
             yColor : '#CCC',
             rectUpColor : '#A0C45E',
             rectDownColor : '#F9653C',
             lineUpColor : '#A0C45E',
             lineDownColor : '#F9653C',
+            dateFormatHour : 'HH:mm',
+            dateFormatDay : 'DD HH:mm',
             tooltipStyle  : null,
             tooltipxStyle : null, 
             tooltipyStyle : null, 
@@ -74,7 +99,9 @@ function p(a){
             timestampMax : 0        
         }
         , chartType = 'candle'
+        , prevClose = null
         , data = null
+        , displayRange = []  //be able to display a part of data 
         , xaxisInterval = []
         , container = null  //only container is a jQuery object
         //All d3js objects in ui
@@ -109,10 +136,27 @@ function p(a){
              throw new ReferenceError('Input container not exist');
         }
 
+        namespace = randomString(10) + '_'; 
+
         initUi(ui);
     }
 
-
+    /**
+     * By James from http://www.xinotes.org/notes/note/515/
+     */
+    function randomString(length) {
+        var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
+       
+        if (! length) {
+            length = Math.floor(Math.random() * chars.length);
+        }
+       
+        var str = '';
+        for (var i = 0; i < length; i++) {
+            str += chars[Math.floor(Math.random() * chars.length)];
+        }
+        return str;
+    }
 
     function initUi(inputUi) {
         var width = options.width,
@@ -141,53 +185,58 @@ function p(a){
             .attr("transform", "translate(" + marginLeft + "," + marginTop + ")");
 
         container.css({
-            position : 'relative'
+            //background : '#EFEFEF',
+            border : '1px solid',
+            position : 'relative',
+            width : width + 'px',
+            height : height + 'px'
         });
 
         ui.tooltip = d3.select(container.get(0)).append("div").attr("class", "evafinance-tooltip");
         ui.tooltipx = d3.select(container.get(0)).append("div").attr("class", "evafinance-tooltipx");
         ui.tooltipy = d3.select(container.get(0)).append("div").attr("class", "evafinance-tooltipy");
+
     }
 
     function drawXaxis() {
         var x = d3.scale.linear()
-            .domain([0, data.length -1])
-            .range([0, status.innerWidth]);
+                .domain([0, data.length -1])
+                .range([0, status.innerWidth]);
 
-        var xDisplay = d3.scale.linear().domain([status.timestampMin, status.timestampMax]);
+        //time diff cross a day
+        //p(moment(status.timestampMax).format('YYYYMMDD') - moment(status.timestampMin).format('YYYYMMDD'));
 
         var xAxis = d3.svg.axis()
             .scale(x)
             .orient("bottom")
+            .tickFormat(function(d) { return moment(data[d].start).format("HH:mm")})
+            .tickSize(options.xAxisTickSize)
             .ticks(options.xAxisTicks);
-            //.ticks(2);
-            //.tickFormat(function(timestamp) { return moment(timestamp).format("HH:mm"); });
-
             
         status.x = x;
 
         ui.chart.append("g")
-        .attr("class", "evachart-xaxis")
+        .attr("class", "evafinance-xaxis")
         .attr("transform", "translate(0," + status.innerHeight + ")")
         .call(xAxis);
 
-        ui.chart.selectAll(".evachart-xaxis text")
-            .attr("font-size", "12px")
-            .attr("fill", "#333");
+        ui.chart.selectAll(".evafinance-xaxis text")
+            .attr("font-size", options.xAxisLabelSize + "px")
+            .attr("fill", options.xAxisLabelColor);
         
         //xAxis.selectAll("text").attr("font-size", "12px");
 
-        ui.chart.selectAll('.evachart-xaxis path, .evachart-xaxis line')
+        ui.chart.selectAll('.evafinance-xaxis path, .evachart-xaxis line')
             .attr('stroke', options.xAxisStroke)
             .attr('shape-rendering', options.xAxisShapeRendering)
             .attr('fill', options.xAxisFill);
 
         ui.chart.append("g")
-        .attr("class", "evachart-xgridlines")
-        .selectAll(".evachart-xgridline")
+        .attr("class", "evafinance-xgridlines")
+        .selectAll(".evafinance-xgridline")
         .data(x.ticks(options.xGridTicks))
         .enter().append("svg:line")
-            .attr("class", "evachart-xgridline")
+            .attr("class", "evafinance-xgridline")
             .attr("x1", x)
             .attr("x2", x)
             .attr("y1", 0)
@@ -201,41 +250,50 @@ function p(a){
     }
 
     function drawYaxis() {
-        var domainDiff = (status.priceMax - status.priceMin) / 20;
-        var y = d3.scale.linear()
-            .domain([
-                Math.ceil(status.priceMin - domainDiff),
-                Math.floor(status.priceMax + domainDiff) //add 10% domain offset
-            ])
-            .range([status.innerHeight, 0]);
+        trigger('evafinance.drawxaxis.before');
 
-        var yAxis = d3.svg.axis()
-            .scale(y)
-            .ticks(options.yAxisTicks)
-            .orient(options.yAxisOrient);
+        var domainDiff = (status.priceMax - status.priceMin) / 20;
+
+        if(status.maxNumLength > 2) {
+            var yAxisMax = status.priceMax + domainDiff, //add 10% domain offset
+            yAxisMin = status.priceMin - domainDiff,
+            yAxisMax = prevClose > 0 && prevClose >= yAxisMax ? prevClose + domainDiff : yAxisMax,
+            yAxisMin = prevClose > 0 && prevClose <= yAxisMin ? prevClose - domainDiff : yAxisMin;
+        } else {
+            var yAxisMax = Math.ceil(status.priceMax + domainDiff), //add 10% domain offset
+            yAxisMin = Math.floor(status.priceMin - domainDiff),
+            yAxisMax = prevClose > 0 && prevClose >= yAxisMax ? Math.ceil(prevClose + domainDiff) : yAxisMax,
+            yAxisMin = prevClose > 0 && prevClose <= yAxisMin ? Math.floor(prevClose - domainDiff) : yAxisMin;
+        }
+
+        var y = d3.scale.linear().domain([yAxisMin, yAxisMax]).range([status.innerHeight, 0]),
+            yAxis = d3.svg.axis().scale(y)
+                    .ticks(options.yAxisTicks)
+                    .tickSize(options.yAxisTickSize)
+                    .orient(options.yAxisOrient);
 
         status.y = y;
 
         ui.chart.append("g")
-            .attr("class", "evachart-yaxis")
+            .attr("class", "evafinance-yaxis")
             .attr("transform", "translate(" + status.innerWidth + ",0)")
             .call(yAxis);
 
-        ui.chart.selectAll('.evachart-yaxis path, .evachart-yaxis line')
+        ui.chart.selectAll('.evafinance-yaxis path, .evachart-yaxis line')
             .attr('stroke', options.yAxisStroke)
             .attr('shape-rendering', options.yAxisShapeRendering)
             .attr('fill', options.yAxisFill);
 
-        ui.chart.selectAll(".evachart-yaxis text")
-            .attr("font-size", "12px")
-            .attr("fill", "#333");
+        ui.chart.selectAll(".evafinance-yaxis text")
+            .attr("font-size", options.yAxisLabelSize + "px")
+            .attr("fill", options.yAxisLabelColor);
 
         ui.chart.append("g")
-        .attr("class", "evachart-ygridlines")
-        .selectAll(".evachart-xgridline")
+        .attr("class", "evafinance-ygridlines")
+        .selectAll(".evafinance-xgridline")
         .data(y.ticks(options.yGridTicks))
         .enter().append("svg:line")
-            .attr("class", "evachart-xgridline")
+            .attr("class", "evafinance-xgridline")
             .attr("x1", 0)
             .attr("x2", status.innerWidth)
             .attr("y1", y)
@@ -244,6 +302,11 @@ function p(a){
             //.attr("stroke-dasharray", "5,5")
             .attr("stroke", options.yGridStroke);
     
+        trigger('evafinance.drawxaxis.after');
+    }
+
+    function trigger(eventName) {
+        $(document).trigger(namespace + eventName, this);
     }
 
     /************************************
@@ -316,6 +379,15 @@ function p(a){
             return data;
         }
 
+        , setPrevClose : function(num){
+            prevClose = num;
+            return this;
+        }
+
+        , getPrevClose : function(){
+            return prevClose;
+        }
+
         , getStatus : function(){
             return status;
         }
@@ -333,6 +405,8 @@ function p(a){
             } else {
                 this.drawAreaChart();
             }
+
+            return this;
         }
 
         , drawCandleChart : function(){
@@ -340,10 +414,66 @@ function p(a){
         }
 
         , drawAreaChart : function(){
-        
+            var area = d3.svg.area()
+                .x(function(d, i) { return status.x(i); })
+                .y0(status.innerHeight)
+                .y1(function(d) { return status.y(d.price); })
+            , line = d3.svg.line()
+                .x(function(d, i) { return status.x(i); })
+                .y(function(d) { return status.y(d.price); })
+            , board = ui.chart.selectAll('g.evafinance-boardarea').empty() ? 
+                ui.chart.append("g").attr('class', 'evafinance-boardarea') : 
+                ui.chart.selectAll('g.evafinance-boardarea');
+
+            //init xInterval whatever
+            xInterval = [];
+
+            board.append("path")
+            .datum(data)
+            .attr("class", "evafinance-chartarea-fill")
+            .attr("d", area)
+            .attr("fill", options.areaFillColor)
+            .attr("opacity", options.areaFillOpacity);
+
+            board.append("path")
+            .datum(data)
+            .attr("class", "evafinance-chartarea-line")
+            .attr("d", line)
+            .attr("fill", 'none')
+            .attr("stroke", options.areaLineColor)
+            .attr("stroke-width", options.areaLineWidth + "px");
+
+            board.selectAll("circle").data(data).enter()
+            .append("circle")
+            .attr("stroke", options.areaPointStroke)
+            .attr("stroke-width", options.areaPointStrokeWidth)
+            .attr("fill", options.areaPointFill)
+            .attr("class", "evafinance-chartarea-circle")
+            .attr("cx", function(d, i) { 
+                var point = status.x(i);
+                xInterval.push(point);
+                return point; 
+            })
+            .attr("cy", function(d, i) { return status.y(d.price) })
+            .attr("r", options.areaPointSize);
+
+            options.areaPointEnable ? '' : board.selectAll("circle").attr("visibility", "hidden");
+            
+
+            return this;
         }
 
-    
+        , bind : function(eventName, func){
+            $(document).on(namespace + eventName, func);
+
+            return this;
+        }
+
+        , unbind : function(eventName) {
+            $(document).off(namespace + eventName);
+
+            return this;
+        }
     }
 
 
